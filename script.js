fb2becf8fdd94ee7205260cef8131ac5f274e249
Akrem_Omer <133592980+Akremomer@users.noreply.github.com>
2026-03-04 14:24:47 +0300

    Enhance toggleMenu and add role cycling feature
    Added functionality to cycle through roles every 2 seconds.

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const roles = [
+  "Fullstack Web Developer",
+  "Software Engineer",
+  "Web Designer"
+];
+
+let index = 0;
+const textElement = document.getElementById("changing-text");
+
+function changeRole() {
+  index = (index + 1) % roles.length;
+  textElement.textContent = roles[index];
+}
+
+setInterval(changeRole, 2000);
+
 
