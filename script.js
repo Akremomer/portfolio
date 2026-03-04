3a91641c4b86bc81e551ea2b11c1f7d288588492
Akrem_Omer <133592980+Akremomer@users.noreply.github.com>
2026-03-04 14:43:08 +0300

    Refactor toggleMenu function in script.js

--- script.js
+++ script.js
@@ -4,29 +4,8 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-document.addEventListener("DOMContentLoaded", function () {
-  const roles = [
-    "Fullstack Web Developer",
-    "Software Engineer",
-    "Web Designer"
-  ];
 
-  let index = 0;
-  const textElement = document.getElementById("changing-text");
 
-  function changeRole() {
-    textElement.classList.add("fade-out");
-
-    setTimeout(() => {
-      index = (index + 1) % roles.length;
-      textElement.textContent = roles[index];
-
-      textElement.classList.remove("fade-out");
-    }, 500);
-  }
-
-  setInterval(changeRole, 3000);
-});
 
 
 
